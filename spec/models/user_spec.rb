b6c2e12321cbfede010f08a23bd4e4f3913f20cf
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-01-24 13:06:24 -0800

    User model tested: validate_length_of email: passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,6 +52,8 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of(:email) }
 
+  it { should validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create) }
+
 
 
 
