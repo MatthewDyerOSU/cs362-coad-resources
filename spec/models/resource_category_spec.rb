46c424d7fb7e21bbed36f8edf5a7f7e8d1111986
theusaf <theusafyt@gmail.com>
2023-01-24 11:21:50 -0800

    test(resource_category): validates name presence

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -16,4 +16,8 @@ RSpec.describe ResourceCategory, type: :model do
 
   it { should have_and_belong_to_many :organizations }
 
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
+
 end
