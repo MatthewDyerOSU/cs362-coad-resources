1518d04333c2ee07eed541f3b2f93afa4683b8f0
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-01-24 13:12:27 -0800

    User model tested: validate_presence_of password on :create: passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,8 +54,11 @@ RSpec.describe User, type: :model do
 
   it { should validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create) }
 
+  # need spec for validates :email, format: { with: VALID_EMAIL_REGEX }
+
   it { should validate_uniqueness_of(:email).case_insensitive }
 
+  it { should validate_presence_of(:password).on(:create) }
 
 
 end
