9e0c387c914d0500e167e5c535aeb1a704a7d65e
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-01-24 13:13:53 -0800

    User model tested: validate_length_of password: passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,5 +60,6 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of(:password).on(:create) }
 
+  it { should validate_length_of(:password).is_at_least(7).is_at_most(255).on(:create) }
 
 end
