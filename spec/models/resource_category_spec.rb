44cd68e11342d6e121add1b745494b215ff2e7ba
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-01-31 11:45:40 -0800

    ResourceCategory: refactoring, removed redundant tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -7,16 +7,6 @@ RSpec.describe ResourceCategory, type: :model do
   it { should have_many :tickets }
   it { should have_and_belong_to_many :organizations }
 
-  it 'responds to name' do
-    expect(active_resource_category).to respond_to(:name)
-  end
-
-  it 'responds to active' do
-    expect(active_resource_category).to respond_to(:active)
-  end
-
-  
-
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
