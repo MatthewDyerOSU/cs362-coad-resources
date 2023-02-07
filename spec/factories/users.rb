6e2cb0c9fa6b0f11144206012e43a7cbec51ce61
theusaf <theusafyt@gmail.com>
2023-02-07 10:06:38 -0800

    style: format users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
-    factory :user do
-      email { Faker::Internet.email }
-      password { Faker::Internet.password(min_length: 7, max_length: 128, mix_case: true) }
-      confirmed_at { Time.now }
-      role { :organization }
-      association :organization, factory: :organization, optional: true
-  
-      trait :admin do
-        role { :admin }
-      end
+  factory :user do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 7, max_length: 128, mix_case: true) }
+    confirmed_at { Time.now }
+    role { :organization }
+    association :organization, factory: :organization, optional: true
+
+    trait :admin do
+      role { :admin }
     end
   end
-  
+end
