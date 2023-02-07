3108f332ef0e34975dedae7d1da12b16f54097f0
theusaf <theusafyt@gmail.com>
2023-02-07 11:32:33 -0800

    test(organization_controller): reorganize tests and add more indexes

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -2,29 +2,42 @@ require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
 
-  describe "GET #new" do
+  let (:user_with_org) { create(:user) }
+  let (:user_without_org) { create(:user, organization: nil) }
 
-    describe "not logged in" do
+  describe "not logged in" do
+    describe "GET #new" do
       it "redirects to login page" do
         get :new
         expect(response).to redirect_to(new_user_session_path)
       end
     end
+    describe "GET #index" do
+      it "redirects to login page" do
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 
-    describe "logged in" do
-      it "redirects to dashboard for users with organizations" do
-        sign_in create(:user)
+  describe "logged in as user with organization" do
+    describe "GET #new" do
+      it "redirects to dashboard" do
+        sign_in user_with_org
         get :new
         expect(response).to redirect_to(dashboard_path)
       end
+    end
+  end
 
-      it "returns http success for users without organizations" do
-        sign_in create(:user, organization: nil)
+  describe "logged in as user without organization" do
+    describe "GET #new" do
+      it "returns http success" do
+        sign_in user_without_org
         get :new
         expect(response).to have_http_status(:success)
       end
     end
-
   end
 
 end
