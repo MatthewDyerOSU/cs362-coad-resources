2c51ba36bb534076e3199f327ec5f077e925dedf
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-02-09 10:47:14 -0800

    committing to pull changes

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
    
+    let (:ticket) { create(:ticket) }
+    let (:user) { create(:user) }
+
     describe "GET #new" do
         it "has http status of success" do
             get :new
@@ -13,6 +16,16 @@ RSpec.describe TicketsController, type: :controller do
         it "has http status of success" do
             post :create, params: { ticket: { name: "Sample Ticket" } }
             expect(response).to have_http_status(:success)
+        end
+    end
+
+    describe "GET #show" do
+        it "has http status of success" do
+            sign_in user
+            get :show, params: { id: ticket.id } 
+            # expect(response).to have_http_status(:success)
+            # expect(response).to redirect_to(ticket_path)
+            expect(response).to redirect_to(dashboard_path)
         end
     end
 
