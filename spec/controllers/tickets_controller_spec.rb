f50b940cf921aad3744432cbc45a9bdc6c39c2c5
Matthew Dyer <dyerma@oregonstate.edu>
2023-02-09 09:20:27 -0800

    nothing changed

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
+    # describe "GET #new" do
+        
+    #     describe "When creating new ticket" do
+    #         it "Displays form for creating ticket" do
+    #             get :new
+    #             expect(response)
+
+
+
+
+
+
+    # end
 end
