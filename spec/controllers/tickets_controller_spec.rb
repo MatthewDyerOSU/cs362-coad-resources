0c03495efbfa975d6e3047f2b8c236fb1fca17de
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-02-09 09:50:37 -0800

    Tickets: controller test POST #create passed

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
-
+   
     describe "GET #new" do
-        
-        describe "When creating new ticket" do
-            it "Displays form for creating ticket" do
-                get :new
-                expect(response).to have_http_status(:success)
-            end
+        it "has http status of success" do
+            get :new
+            expect(response).to have_http_status(:success)
         end
-
-
-
-
-
     end
+
+    describe "POST #create" do
+        it "redirects to tickets_path" do
+            post :create, params: { ticket: { name: "Sample Ticket" } }
+            expect(response).to have_http_status(:success)
+        end
+    end
+
 end
