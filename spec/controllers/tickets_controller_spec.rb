91786903c6100b760c8fe8f5cb1c213d14edea3d
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-02-09 11:11:09 -0800

    Ticket: controller test GET #show passed for user with approved organization

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe TicketsController, type: :controller do
    
     let (:ticket) { create(:ticket) }
-    let (:user) { create(:user) }
+    let (:approved_organization) { create(:organization, status: :approved) }
+    let (:user_without_org) { create(:user) }
     let (:admin_user) { create(:user, :admin) }
+    let (:user_with_org) { create(:user, organization: approved_organization) }
 
     describe "GET #new" do
         it "has http status of success" do
@@ -23,7 +25,7 @@ RSpec.describe TicketsController, type: :controller do
     describe "GET #show" do
         describe "User without organization" do
             it "redirects to dashboard" do
-                sign_in user
+                sign_in user_without_org
                 get :show, params: { id: ticket.id } 
                 expect(response).to redirect_to(dashboard_path)
             end
@@ -36,6 +38,15 @@ RSpec.describe TicketsController, type: :controller do
                 expect(response).to have_http_status(:success)
             end
         end
+
+        describe "User with approved organization" do
+            it "has http status of success" do
+                sign_in user_with_org
+                get :show, params: { id: ticket.id }
+                expect(response).to have_http_status(:success)
+            end
+        end
+
     end
 
 end
