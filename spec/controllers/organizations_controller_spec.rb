007359267d75cae5d648b51cf8d8c93d2e67b475
theusaf <theusafyt@gmail.com>
2023-02-09 10:13:37 -0800

    test(organization_controller): add index specs

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe OrganizationsController, type: :controller do
 
   let (:user_with_org) { create(:user) }
   let (:user_without_org) { create(:user, organization: nil) }
+  let (:admin_user) { create(:user, :admin) }
 
   describe "GET #new" do
     describe "not logged in" do
@@ -26,13 +27,41 @@ RSpec.describe OrganizationsController, type: :controller do
         expect(response).to have_http_status(:success)
       end
     end
+    describe "logged in as admin" do
+      it "redirects to dashboard" do
+        sign_in admin_user
+        get :new
+        expect(response).to redirect_to(dashboard_path)
+      end
+    end
   end
 
   describe "GET #index" do
     describe "not logged in" do
       it "redirects to login page" do
-        get :new
+        get :index
         expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    describe "logged in as user with organization" do
+      it "returns http success" do
+        sign_in user_with_org
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+    describe "logged in as user without organization" do
+      it "returns http success" do
+        sign_in user_without_org
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+    describe "logged in as admin" do
+      it "returns http success" do
+        sign_in admin_user
+        get :index
+        expect(response).to have_http_status(:success)
       end
     end
   end
