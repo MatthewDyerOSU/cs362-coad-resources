7d1a83c0d200dd6ee470ce056bb210f32581290b
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-02-09 09:34:43 -0800

    Tickets: controller test for get new ticket passed

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
 
-    # describe "GET #new" do
+    describe "GET #new" do
         
-    #     describe "When creating new ticket" do
-    #         it "Displays form for creating ticket" do
-    #             get :new
-    #             expect(response)
+        describe "When creating new ticket" do
+            it "Displays form for creating ticket" do
+                get :new
+                expect(response).to have_http_status(:success)
+            end
+        end
 
 
 
 
 
-
-    # end
+    end
 end
