ad240b4cf390a58ffdd7016254866c8dc96a8adf
theusaf <theusafyt@gmail.com>
2023-02-10 09:40:07 -0800

    test(static_pages): add index test

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
+  describe "GET index" do
+    it "is successful" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
 end
