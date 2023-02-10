96a564a8a783be389252b71cbf9eccc8842409ca
theusaf <theusafyt@gmail.com>
2023-02-10 09:47:16 -0800

    test(static_pages): add org submission test

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -20,4 +20,12 @@ RSpec.describe StaticPagesController, type: :controller do
     end
   end
 
+  describe "GET #organization_application_submitted" do
+    # While the routes.rb redirects to the login page if the user is not logged in,
+    # the controller action does not. We do not test for this here.
+    it "is successful" do
+      expect(get :organization_application_submitted).to be_successful
+    end
+  end
+
 end
