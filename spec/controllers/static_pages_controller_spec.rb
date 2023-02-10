55ab6f5e7b50285ee28acb2d7e98995057a9d727
theusaf <theusafyt@gmail.com>
2023-02-10 09:41:37 -0800

    test(static_pages): add org expectations submitted

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -4,13 +4,19 @@ RSpec.describe StaticPagesController, type: :controller do
 
   describe "GET #index" do
     it "is successful" do
-      expect(get :index).to be_success
+      expect(get :index).to be_successful
     end
   end
 
   describe "GET #ticket_submitted" do
     it "is successful" do
-      expect(get :ticket_submitted).to be_success
+      expect(get :ticket_submitted).to be_successful
+    end
+  end
+
+  describe "GET #organization_expectations" do
+    it "is successful" do
+      expect(get :organization_expectations).to be_successful
     end
   end
 
