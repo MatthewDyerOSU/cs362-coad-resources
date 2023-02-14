19e9c0ec6329cc8f5da3e5843bd7f4358fe68983
theusaf <theusafyt@gmail.com>
2023-02-14 10:12:47 -0800

    test(regions_controller/admin): test getting index

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
+  let (:admin) { create(:user, :admin) }
+
   describe('User is not logged in') do
     it 'redirects to login page' do
       expect(get :index).to redirect_to(new_user_session_path)
@@ -24,6 +26,19 @@ RSpec.describe RegionsController, type: :controller do
       expect(get :edit, params: { id: 1 }).to redirect_to(dashboard_path)
       expect(patch :update, params: { id: 1 }).to redirect_to(dashboard_path)
       expect(delete :destroy, params: { id: 1 }).to redirect_to(dashboard_path)
+    end
+  end
+
+  describe('User is logged in as admin') do
+
+    before do
+      sign_in admin
+    end
+
+    describe('GET #index') do
+      it('is successful for index') do
+        expect(get :index).to be_successful
+      end
     end
   end
 
