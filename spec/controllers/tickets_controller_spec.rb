e07ce88058389a265b1670ab2a1f60ebf2ba9fb5
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-02-14 07:50:18 -0800

    Tickets: reformatted spec file to have 3 groups representing each user and then passed user without org GET #show test

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -8,57 +8,81 @@ RSpec.describe TicketsController, type: :controller do
     let (:admin_user) { create(:user, :admin) }
     let (:user_with_org) { create(:user, organization: approved_organization) }
 
-    describe "GET #new" do
-        it "has http status of success" do
-            get :new
-            expect(response).to have_http_status(:success)
+    
+    describe "User is an admin" do
+        before(:each) do 
+            sign_in admin_user
+        end
+
+        describe "GET #new" do
+            it "has http status of success" do
+                get :new
+                expect(response).to have_http_status(:success)
+            end
+        end
+
+        describe "POST #create" do
+            it "has http status of success" do
+                post :create, params: { ticket: { name: "Sample Ticket" } }
+                expect(response).to have_http_status(:success)
+            end
+        end
+
+        describe "GET #show" do
+            it "has http status of success" do
+                get :show, params: { id: ticket.id }
+                expect(response).to have_http_status(:success)
+            end
+        end
+
+        describe "POST #capture" do
+            it "has http status of success" do
+                post :capture, params: { id: ticket.id }
+                expect(response).to redirect_to(dashboard_path)
+            end
         end
     end
 
-    describe "POST #create" do
-        it "has http status of success" do
-            post :create, params: { ticket: { name: "Sample Ticket" } }
-            expect(response).to have_http_status(:success)
+    describe "Non admin user without organization" do
+        before(:each) do
+            sign_in user_without_org
         end
-    end
 
-    describe "GET #show" do
-        describe "User without organization" do
+        describe "GET #new" do
+            it "has http status of success" do
+                get :new
+                expect(response).to have_http_status(:success)
+            end
+        end
+
+        describe "GET #show" do
             it "redirects to dashboard" do
-                sign_in user_without_org
                 get :show, params: { id: ticket.id } 
                 expect(response).to redirect_to(dashboard_path)
             end
         end
 
-        describe "User as admin" do
-            it "has http status of success" do
-                sign_in admin_user
-                get :show, params: { id: ticket.id }
-                expect(response).to have_http_status(:success)
-            end
-        end
-
-        describe "User with approved organization" do
-            it "has http status of success" do
-                sign_in user_with_org
-                get :show, params: { id: ticket.id }
-                expect(response).to have_http_status(:success)
-            end
-        end
-    end
-
-    describe "POST #capture" do
-        describe "User without organization" do
+        describe "POST #capture" do
             it "redirects to dashboard" do
-                sign_in user_without_org
                 post :capture, params: { id: ticket.id }
                 expect(response).to redirect_to(dashboard_path)
             end
         end
+    end
 
-        # this needs addressing
-        describe "User with approved organization" do
+    describe "Non admin user with approved organization" do
+        before(:each) do
+            sign_in user_with_org
+        end
+
+        describe "GET #show" do
+            it "has http status of success" do
+                get :show, params: { id: ticket.id }
+                expect(response).to have_http_status(:success)
+            end
+        end
+
+        describe "POST #capture" do
             it "has http status of success" do
                 sign_in user_with_org
                 post :capture, params: { id: ticket.id }
