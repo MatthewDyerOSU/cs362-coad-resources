7b7283fef70b4ccc7d9eb41cd2645f7a8643f50f
theusaf <theusafyt@gmail.com>
2023-02-14 10:08:55 -0800

    test(regions_controller): test non-admin user

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe RegionsController, type: :controller do
     end
   end
 
+  describe('User is logged in as non-admin') do
+    it 'redirects to dashboard' do
+      sign_in(create(:user))
+      expect(get :index).to redirect_to(dashboard_path)
+      expect(get :show, params: { id: 1 }).to redirect_to(dashboard_path)
+      expect(get :new).to redirect_to(dashboard_path)
+      expect(post :create).to redirect_to(dashboard_path)
+      expect(get :edit, params: { id: 1 }).to redirect_to(dashboard_path)
+      expect(patch :update, params: { id: 1 }).to redirect_to(dashboard_path)
+      expect(delete :destroy, params: { id: 1 }).to redirect_to(dashboard_path)
+    end
+  end
+
 end
