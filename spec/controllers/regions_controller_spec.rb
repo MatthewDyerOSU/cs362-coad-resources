08d214315b1d595773eb0801678255ee89f2b2b7
theusaf <theusafyt@gmail.com>
2023-02-14 10:06:36 -0800

    test(regions_controller): test login

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
+  describe('User is not logged in') do
+    it 'redirects to login page' do
+      expect(get :index).to redirect_to(new_user_session_path)
+      expect(get :show, params: { id: 1 }).to redirect_to(new_user_session_path)
+      expect(get :new).to redirect_to(new_user_session_path)
+      expect(post :create).to redirect_to(new_user_session_path)
+      expect(get :edit, params: { id: 1 }).to redirect_to(new_user_session_path)
+      expect(patch :update, params: { id: 1 }).to redirect_to(new_user_session_path)
+      expect(delete :destroy, params: { id: 1 }).to redirect_to(new_user_session_path)
+    end
+  end
+
 end
