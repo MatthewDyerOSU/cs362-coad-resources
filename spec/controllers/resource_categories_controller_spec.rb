21bd7228d521704cdd076ebd2dacf653a17b85d8
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-02-14 11:35:40 -0800

    Resource_categories: POST #create redirects passed for user with approved org

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -67,6 +67,13 @@ RSpec.describe ResourceCategoriesController, type: :controller do
                 expect(response).to redirect_to(dashboard_path)
             end
         end
+
+        describe "POST #create" do
+            it "redirects to dashboard" do
+                post :create, params: { resource_category: { name: "Sample Resource Category" } }
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
     end
 
     describe "User without approved organization" do
