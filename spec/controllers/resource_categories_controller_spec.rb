1198d7742a113910d6a8dbc64d58209639b659b5
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-02-16 09:48:21 -0800

    Resource_categories: PATCH #activate redirects passed for user with approved org

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -96,6 +96,13 @@ RSpec.describe ResourceCategoriesController, type: :controller do
                 expect(response).to redirect_to(dashboard_path)
             end
         end
+
+        describe "PATCH #activate" do
+            it "redirects to dashboard" do
+                patch :activate, params: { id: resource_category.id }
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
     end
 
     describe "User without approved organization" do
