a5de52de6022ad0405bc748ebc4aedd0931dd951
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-02-16 10:26:38 -0800

    Resource_categories: DELETE #destroy redirects correctly for user with approved org

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -138,6 +138,13 @@ RSpec.describe ResourceCategoriesController, type: :controller do
                 expect(response).to redirect_to(dashboard_path)
             end
         end
+
+        describe "DELETE #destroy" do
+            it "redirects to resource_categories_path" do
+                delete :destroy, params: { id: resource_category.id }
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
     end
 
     describe "User without approved organization" do
