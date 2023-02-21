5158f7b0b3c254879098e7fe9a4303d0a300e814
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-02-21 11:34:27 -0800

    Application_helper_spec.rb: returns the base title when given an empty string: passed

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-
+    describe "#full_title" do
+        it "returns the base title when given an empty string" do
+          expect(helper.full_title).to eq("Disaster Resource Network")
+        end
+    
+        
+      end
+    
 end
