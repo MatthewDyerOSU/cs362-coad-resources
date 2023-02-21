f5b1a0075a5268e28c85dbe1ce2f6236e1d8cdc0
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-02-21 11:35:48 -0800

    Application_helper_spec.rb: returns the full title when given a page title: passed

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe ApplicationHelper, type: :helper do
         it "returns the base title when given an empty string" do
           expect(helper.full_title).to eq("Disaster Resource Network")
         end
-    
+        it "returns the full title when given a page title" do
+            expect(helper.full_title("About Us")).to eq("About Us | Disaster Resource Network")
+          end
         
       end
     
