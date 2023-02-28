265567bf3db0aa8f4d9a85ae35a17e1dc1db664c
theusaf <theusafyt@gmail.com>
2023-02-28 10:43:37 -0800

    test(mailers): add tests for mailers

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,4 +2,23 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
 
+  describe "development" do
+    it 'returns false' do
+      allow_any_instance_of(UserMailer).to receive(:params).and_return({to: ""})
+      allow(Rails.env).to receive(:production?).and_return(false)
+      allow(Rails.env).to receive(:test?).and_return(false)
+      mailer = UserMailer.new
+      expect(mailer.new_organization_application).to be_falsey
+    end
+  end
+
+  describe "production" do
+    it 'sends an email' do
+      allow_any_instance_of(UserMailer).to receive(:mail).and_return(true)
+      allow_any_instance_of(UserMailer).to receive(:params).and_return({to: ""})
+      mailer = UserMailer.new
+      expect(mailer.new_organization_application).to be_truthy
+    end
+  end
+
 end
