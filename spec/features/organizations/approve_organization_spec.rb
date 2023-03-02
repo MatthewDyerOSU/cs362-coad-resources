931611c6a4f17eecd82c295fd200f9bfaba95602
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-03-02 10:37:20 -0800

    ticket_service test still needs fixing

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
 
+    it 'can be done from the dashboard' do
+        user = create(:user, :admin)
+        organization = create(:organization)
+        log_in_as(user)
+
+        visit dashboard_path  # Navigate to the dashboard
+
+        click_on 'Organizations'
+        click_on 'Pending'
+        click_on 'Review'
+        click_on 'Approve'
+
+        expect(organization.reload.approved?).to be true
 end
--- spec/services/ticket_service_spec.rb
+++ spec/services/ticket_service_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe TicketService do
             expect(TicketService.release_ticket(1, user)).to eq(:ok)
         end
     end
+    describe '#capture_ticket' do
+        let(:organization) { double('organization', id: 'some_org_id', approved?: true) }
+        let(:user) { double('user', organization: organization) }
+        let(:ticket) { double('ticket', organization_id: nil, save: true) }
+      
+        before do
+          allow(Ticket).to receive(:find).and_return(ticket)
+          allow(ticket).to receive(:save).and_return(true)
+        end
+      
+        it 'returns :ok when ticket successfully captured' do
+          expect(ticket).to receive(:organization_id=).with('some_org_id')
+          expect(TicketService.capture_ticket(1, user)).to eq(:ok)
+        end
+    end
+      
+      
 end
