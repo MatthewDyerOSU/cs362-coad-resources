3fc19285fb10b33f285c955685b14a6c207edad7
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-03-09 09:52:10 -0800

    registrations_controller_spec: when recaptcha verified: does not render new view: passed

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Users::RegistrationsController, type: :controller do
-  skip
+  describe '#check_captcha' do
+    before { allow(controller).to receive(:verify_recaptcha).and_return(true) }
+    let(:sign_up_params) { {email: 'text@example.com', password: 'password' } }
+    let(:resource) { double(:resource) }
+    let(:resource_class) { double(:resource_class, new: resource) }
+
+    context 'when recaptcha is verified' do
+      it 'does not render new view' do
+        expect(controller).not_to receive(:render).with(:new)
+        controller.send(:check_captcha)
+      end
+    end
+  end
 end
