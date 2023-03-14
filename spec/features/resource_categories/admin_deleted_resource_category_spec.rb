132ff01e2842d2307374ed8763a136599e50a8aa
theusaf <theusafyt@gmail.com>
2023-03-14 10:40:52 -0700

    test(features/resource_category/delete): test that admins can delete categories, but not regular users

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
 
+  it 'can be done by an admin' do
+    admin = create(:user, :admin)
+    resource_category = create(:resource_category, name: 'Test')
+
+    log_in_as(admin)
+    visit dashboard_path
+    click_on 'Categories'
+    click_on 'Test'
+    click_on 'Delete'
+    expect(page).to have_content('Category Test was deleted.')
+  end
+
+  it 'cannot be done by a non-admin' do
+    user = create(:user)
+    resource_category = create(:resource_category, name: 'Test')
+
+    log_in_as(user)
+    visit dashboard_path
+    within('#navbar') do
+      expect(page).not_to have_content('Categories')
+    end
+  end
+
 end
