9523ba54c2d69d13cad54e1fd46695dac6d9564f
theusaf <theusafyt@gmail.com>
2023-03-14 10:56:18 -0700

    test(features/tickets/delete): test that ticket delete can only be done by an admin

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -2,4 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
 
+  it 'can be done by an admin' do
+    admin = create(:user, :admin)
+    ticket = create(
+      :ticket,
+      name: 'Test',
+      region: create(:region),
+      resource_category: create(:resource_category)
+    )
+
+    log_in_as(admin)
+    visit dashboard_path
+    click_on 'Test'
+    click_on 'Delete'
+    expect(page).to have_content(/Ticket \d+ was deleted\./)
+    within('.list-group') do
+      expect(page).not_to have_content('Test')
+    end
+  end
+
+  it 'cannot be done by a non-admin' do
+    user = create(:user, :approved_org)
+    ticket = create(
+      :ticket,
+      name: 'Test',
+      region: create(:region),
+      resource_category: create(:resource_category)
+    )
+
+    log_in_as(user)
+    visit dashboard_path
+    click_on 'Test'
+    expect(page).not_to have_content('Delete')
+  end
+
 end
