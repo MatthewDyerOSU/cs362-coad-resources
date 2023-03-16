22d83ebaad3025adb945d2d304655e24b3c651b8
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-03-16 12:52:17 -0700

    close_ticket_spec: non admin not able to close ticket: passed

--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -15,4 +15,16 @@ RSpec.describe 'Closing a ticket', type: :feature do
         expect(page).to have_css("dd.col-9", text: "Closed")
     end
 
+    
+    let(:org) { create(:organization, :approved)}
+    let(:user) { create(:user, organization: org) }
+    it 'non admin not able to close ticket' do
+        region = create(:region, name: 'Bend')
+        resource_category = create(:resource_category, name: 'Test Resource Category')
+        ticket = create(:ticket, region: region, resource_category: resource_category)
+        log_in_as(user)
+        visit dashboard_path
+        find('a[href^="/tickets/"]').click
+        expect(page).to_not have_text('Close')
+    end
 end
