216ade549915082cf85f3776b490a5f3180df2b1
theusaf <theusafyt@gmail.com>
2023-03-16 11:52:54 -0700

    test(feature/user/user_registration): verify error when passwords do not match during signup

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
 
+  describe 'invalid password' do
+
+    before :each do
+      allow_any_instance_of(Users::RegistrationsController).to receive(:verify_recaptcha).and_return(true)
+      visit root_path
+      click_on 'Sign up'
+      fill_in 'Email address', with: 'test@example.com'
+    end
+
+    it 'fails if passwords do not match' do
+      fill_in 'Password', with: '1234567'
+      fill_in 'Password confirmation', with: '12345678'
+      within('#new_user') do
+        click_on 'Sign up'
+      end
+      expect(page).to have_content('Password confirmation doesn\'t match Password')
+    end
+  end
+
 end
