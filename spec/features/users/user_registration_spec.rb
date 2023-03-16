dd15bf365c28dc3d3ed56d61692d71bd74b1ca85
theusaf <theusafyt@gmail.com>
2023-03-16 12:00:24 -0700

    test(feature/user/user_registration): add spec for valid registration

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -30,4 +30,22 @@ RSpec.describe 'User registration', type: :feature do
     end
   end
 
+  describe 'valid sign up credentials' do
+
+    it 'succeeds' do
+      allow_any_instance_of(Users::RegistrationsController).to receive(:verify_recaptcha).and_return(true)
+      visit root_path
+      click_on 'Sign up'
+      fill_in 'Email address', with: 'test@example.com'
+      fill_in 'Password', with: '1234567'
+      fill_in 'Password confirmation', with: '1234567'
+      within('#new_user') do
+        click_on 'Sign up'
+      end
+      expect(page).to have_content('A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.')
+      expect(page).to have_current_path(root_path)
+    end
+
+  end
+
 end
