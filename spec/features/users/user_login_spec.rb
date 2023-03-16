f557817bf4789fc15fe12725bc4d39bcdbaed985
theusaf <theusafyt@gmail.com>
2023-03-16 10:29:15 -0700

    test(feature/user/login): test that a user can log in with invalid/valid credentials

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
 
+  before :each do
+    @user = create(:user, email: 'test@example.com', password: 'password123')
+    visit root_path
+    click_on 'Log in'
+  end
+
+  it 'fails with invalid credentials' do
+    fill_in 'Email address', with: 'test@example.com'
+    fill_in 'Password', with: 'wrongpassword'
+    click_on 'Sign in'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
+  it 'succeeds with valid credentials' do
+    fill_in 'Email address', with: 'test@example.com'
+    fill_in 'Password', with: 'password123'
+    click_on 'Sign in'
+    expect(page).to have_content('Signed in successfully.')
+    expect(page).to have_current_path(dashboard_path)
+  end
+
 end
