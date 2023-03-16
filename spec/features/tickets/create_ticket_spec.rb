b57472d5b53cc954f5bdcfad58d078323223ec69
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-03-16 11:40:45 -0700

    create_ticket_spec: fails to create ticket when failing to make all selections: passed

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe 'Creating a Ticket', type: :feature do
         click_on 'Send this help request'
         expect(current_path).to eq ticket_submitted_path
     end
+
+    it 'fails to create ticket when failing to make all selections' do
+        visit new_ticket_path
+        fill_in 'ticket_name', with: 'Test Name'
+        fill_in 'ticket_phone', with: '5555555555'
+        fill_in 'ticket_description', with: 'Test Description'
+        click_on 'Send this help request'
+        expect(page).to have_content('Region must exist')
+        expect(page).to have_content('Resource category must exist')
+        expect(current_path).to_not eq ticket_submitted_path
+    end
 end
