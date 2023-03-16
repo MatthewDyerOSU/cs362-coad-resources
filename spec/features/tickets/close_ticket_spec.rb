75a5ce8960b5d7eb5605a1b3116a4ceb5e7f5783
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-03-16 12:25:32 -0700

    close_ticket_spec: admin successfully closes ticket: passed

--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Closing a ticket', type: :feature do
     let(:admin) { create(:user, :admin)}
 
-    it 'successfully closes ticket' do
+    it 'admin successfully closes ticket' do
+        region = create(:region, name: 'Bend')
+        resource_category = create(:resource_category, name: 'Test Resource Category')
+        ticket = create(:ticket, region: region, resource_category: resource_category)
         log_in_as(admin)
-
         visit dashboard_path
-        
-
+        find('a[href^="/tickets/"]').click
+        click_on 'Close'
+        find('a[href^="/tickets/"]').click
+        expect(page).to have_css("dd.col-9", text: "Closed")
     end
 
 end
